c90d02c2d7c7ec700a147163eef20494cb35add7
Sweetpotatopizza <juhyunlim77@gmail.com>
2021-03-26 22:18:16 +0900

    upload files

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Annie",
+    username: "FemaleTitan",
+    email: "annielove@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Armin",
+    username: "ColosalTitan",
+    email: "Talking@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Jean",
+    username: "Realistic",
+    email: "human@gmail.com",
+  },
+  {
+    id: 4,
+    name: "Sasha",
+    username: "Potato",
+    email: "Potato@gmail.com",
+  },
+  {
+    id: 5,
+    name: "Levi",
+    username: "God",
+    email: "savage@gmail.com",
+  },
+];
